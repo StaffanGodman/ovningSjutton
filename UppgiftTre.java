76c767a3b9c5e1e4d2aef32afc6ca4712ad1eac6
Staffan <staffan.godman@gmail.com>
2020-10-27 11:27:58 +0100

    Add solutions for exercise 17

---
+++ UppgiftTre.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class UppgiftTre {
+    public static void main(String[] args) {
+        List<String> gamblers = new ArrayList<>();
+        System.out.println("Skriv in alla personer som deltar i tävlingen. Avsluta med enter.");
+        String input;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                input = scanner.nextLine();
+                if (input.isEmpty()) {
+                    break;
+                } else {
+                    gamblers.add(input);
+                }
+            }
+        }
+        int winner = ThreadLocalRandom.current().nextInt(gamblers.size());
+        int i = 0;
+        for (String gambler : gamblers) {
+            if (i == winner) {
+
+                System.out.printf("And the winner is: %s !!!", gambler);
+                break;
+            } else {
+                i++;
+            }
+        }
+    }
+}
